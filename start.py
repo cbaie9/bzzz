9fdb8712c83e79cceafa29ef24c89a6d1b5b8acc
cbaie9 <75169332+cbaie9@users.noreply.github.com>
2025-12-11 16:09:26 +0100

    Add files via upload

--- start.py
+++ start.py
@@ -1,1 +1,21 @@
-import tkteasy
+import tkiteasy as tke
+ymax = 600
+xmax = 800
+largeurcarre = 100 
+# Ouverture de fenêtre
+g = tke.ouvrirFenetre(xmax, ymax)
+
+# Votre programme ICI
+
+g.dessinerLigne(0,0,xmax,ymax,'VioletRed')
+g.dessinerLigne(0,ymax/2,xmax,ymax/2,'VioletRed')
+g.dessinerLigne(0,ymax,xmax,0,'red')
+g.dessinerLigne(xmax/2-largeurcarre*0.5,ymax/2-largeurcarre*0.5,xmax/2+largeurcarre*0.5,ymax/2-largeurcarre*0.5,"blue")
+g.dessinerLigne(xmax/2-largeurcarre*0.5,ymax/2+largeurcarre*0.5,xmax/2+largeurcarre*0.5,ymax/2+largeurcarre*0.5,"blue")
+g.dessinerLigne(xmax/2-largeurcarre*0.5,ymax/2-largeurcarre*0.5,xmax/2-largeurcarre*0.5,ymax/2+largeurcarre*0.5,"blue")
+g.dessinerLigne(xmax/2+largeurcarre*0.5,ymax/2-largeurcarre*0.5,xmax/2+largeurcarre*0.5,ymax/2-largeurcarre*0.5,"blue")
+# Boucle à vide qui attend un clic
+g.attendreClic()
+
+# Fermeture fenêtre
+g.fermerFenetre()
